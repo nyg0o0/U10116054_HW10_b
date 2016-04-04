f0ee12efb37e60e9e810e76bc6417fc70db1e3bf
nyg0o0 <yaya56790@gmail.com>
2016-04-04 19:05:54 +0800

    comment

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,29 +2,41 @@ import java.util.*;
  
 public class BubbleSort{
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
- 
-        //int[] arr = {93, 5, 3, 55, 57, 7, 2 ,73, 41, 91};
-		System.out.print("How many numbers for sorting? "); 
-		int numbers = input.nextInt();
 		
-		int[] arr = new int[numbers];
-		for(int i=0; i<numbers;i++){
-			System.out.printf("Elements %d of the array: ",i); 
-			arr[i] = input.nextInt();
-		}
-		
-		System.out.println("---------------------------------");
-		
-        System.out.print("The array before sorting: "); 
-        for(int i = 0; i < arr.length; i++) 
-            System.out.print(arr[i] + ","); 
-		System.out.println(); 
- 
-       
- 
-        System.out.print("The array after sorting: "); 
-        for(int i = 0; i < arr.length; i++) 
-            System.out.print(arr[i] + ",");
+			/* prompt the user to add a array */
+			Scanner input = new Scanner(System.in);	// new object for scanner
+			
+			System.out.print("How many numbers for sorting? "); 	// prompt user to input how many numbers for sorting
+			int numbers = input.nextInt();	// read the input
+			int[] arr = new int[numbers];	// creat a array for specified length
+			
+			for(int i=0; i<numbers;i++){	// store the input data into the array
+				System.out.printf("Elements %d of the array: ",i); 
+				arr[i] = input.nextInt();
+			}
+			
+			
+			System.out.println("---------------------------------");	// divider line
+			
+			
+			/* show the array before sorting */
+			System.out.print("The array before sorting: ");
+			for(int i = 0; i < arr.length; i++){
+				System.out.print(arr[i] + ","); 
+			}
+			
+			/* sort the array */
+			int bubble;
+			for(int i = 0; i < arr.length; i++){
+				System.out.print(arr[i] + ","); 
+			}
+			System.out.println(); 
+	 
+		   
+			/* show the array after sorting */
+			System.out.print("The array after sorting: "); 
+			for(int i = 0; i < arr.length; i++){
+				System.out.print(arr[i] + ",");
+			}
 		}
 } 
